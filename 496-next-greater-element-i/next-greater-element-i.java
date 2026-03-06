d119ae49ee0cbde36065340a0c3e03115a3fd639
Km-reshma <RESHMAK0811@GMAIL.COM>
2026-03-06 19:14:51 +0530

    Time: 4 ms (73.47%) | Memory: 45.3 MB (59.46%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,32 +1,44 @@
+import java.util.*;
+
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-
-        int n2 = nums2.length - 1;
-        int n1 = nums1.length;
-
+        
+        int n = nums2.length;
         Stack<Integer> stack = new Stack<>();
-        HashMap<Integer,Integer> map = new HashMap<>();
-
-        for(int i = n2; i >= 0; i--){
-
-            while(!stack.isEmpty() && stack.peek() <= nums2[i]){
-                stack.pop();
-            }
-
-            if(stack.isEmpty())
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for(int i = n - 1; i >= 0; i--) {
+            
+            if(stack.isEmpty()) {
                 map.put(nums2[i], -1);
-            else
+            }
+            
+            else if(!stack.isEmpty() && stack.peek() > nums2[i]) {
                 map.put(nums2[i], stack.peek());
-
+            }
+            
+            else if(!stack.isEmpty() && stack.peek() <= nums2[i]) {
+                
+                while(!stack.isEmpty() && stack.peek() <= nums2[i]) {
+                    stack.pop();
+                }
+                
+                if(stack.isEmpty()) {
+                    map.put(nums2[i], -1);
+                } else {
+                    map.put(nums2[i], stack.peek());
+                }
+            }
+            
             stack.push(nums2[i]);
         }
-
-        int[] ans = new int[n1];
-
-        for(int i = 0; i < n1; i++){
+        
+        int[] ans = new int[nums1.length];
+        
+        for(int i = 0; i < nums1.length; i++) {
             ans[i] = map.get(nums1[i]);
         }
-
+        
         return ans;
     }
 }
